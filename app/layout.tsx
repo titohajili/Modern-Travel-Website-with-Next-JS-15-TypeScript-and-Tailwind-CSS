a5472127d312bfd733553923795b87bb28e49184
titohajili <maqatoto963@gmail.com>
2025-04-13 16:26:50 +0400

    Setup

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import {Poppins} from "next/font/google";
+//import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const font = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+})
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata: Metadata = {
+  title: "Travel Next js 15",
+  description: "Travel Landing Page",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={` ${font.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
