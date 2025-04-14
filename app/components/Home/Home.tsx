d45193e7b647b282d1aeba528354d6254bbb9605
titohajili <maqatoto963@gmail.com>
2025-04-15 02:38:19 +0400

    Mobile Nav

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='overflow-hidden'>
-      h
+    <div className='overflow-hiddeny'>
+      
     </div>
   )
 }
--- app/components/Home/Navbar/Nav.tsx
+++ app/components/Home/Navbar/Nav.tsx
@@ -1,5 +1,4 @@
 import { navLinks } from '@/app/constant/constand'
-import { link } from 'fs'
 import Link from 'next/link'
 import React from 'react'
 import { HiBars3BottomRight } from 'react-icons/hi2'
@@ -7,7 +6,7 @@ import { TbAirBalloon } from 'react-icons/tb'
 
 const Nav = () => {
   return (
-    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000]'>
+    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000] fixed w-full'>
       <div className='flex justify-between items-center h-full w-[90%] xl:w-[80%] mx-auto'>
         <div className='flex items-center space-x-2'>
           <div className='w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center flex-col'>
