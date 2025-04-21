0da86fb5536f17f6a3128d78985518e87230744f
titohajili <maqatoto963@gmail.com>
2025-04-21 14:31:44 +0400

    Text content

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -5,7 +5,16 @@ const Hero = () => {
     <div className='realtive w-full h-[120vh] sm:h-[100vh]'>
         {/* overlay */}
       <div className='absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70'></div>
+      {/* video */}
       <video src='/images/hero1.mp4' autoPlay muted loop preload='metadata' className='w-full h-full object-cover' />
+      {/* Text content */}
+      <div className='absolute z-[100] w-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+        <div className='flex items-center justify-center flex-col w-full h-full '>
+          <div>
+            <h1 className='text-[25%] mb-4 md:mb-0 text-center md:text-[35px] lg:text-[45px] tracking-[0.7rem] text-white font-bold uppercase' >Lets Enjoy The Nature</h1>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
