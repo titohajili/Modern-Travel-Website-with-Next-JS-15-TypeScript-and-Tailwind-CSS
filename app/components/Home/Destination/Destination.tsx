40f6ae34979ab0584ff4778c11c60a7aa76cf14f
titohajili <maqatoto963@gmail.com>
2025-04-24 17:46:51 +0400

    Section Heading

---
+++ app/components/Home/Destination/Destination.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionHeading from '../../Helper/SectionHeading'
+
+const Destination = () => {
+  return (
+    <div className='pt-20 pb-20'>
+      <SectionHeading heading='Exploring Popular Destinations' />
+    </div>
+  )
+}
+
+export default Destination
--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Hero/Hero'
+import Destination from './Destination/Destination'
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
       <Hero />
+      <Destination />
     </div>
   )
 }
