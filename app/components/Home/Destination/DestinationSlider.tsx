fcade677e4c339ff9064446f1c0b803a49d10413
titohajili <maqatoto963@gmail.com>
2025-04-28 16:38:50 +0400

    Image Text Content

--- app/components/Home/Destination/DestinationSlider.tsx
+++ app/components/Home/Destination/DestinationSlider.tsx
@@ -39,6 +39,9 @@ const DestinationSlider = () => {
               {/* Image */}
               <Image src={data.image} alt={data.country} width={500} height={500} className='h-full w-full object-cover rounded-lg' />
             </div>
+              {/* Text content */}
+              <h1 className='text-lg font-semibold mt-4'>{data.country}</h1>
+              <p className='text-sm text-gray-600'>{data.travelers} Travelers</p>
         </div>
     })}
   </Carousel>
