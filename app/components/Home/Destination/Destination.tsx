e283b520d8e7ff72f9488518f1dbd08fe1b3d129
titohajili <maqatoto963@gmail.com>
2025-04-28 16:34:16 +0400

    Scroll Overlay Image

--- app/components/Home/Destination/Destination.tsx
+++ app/components/Home/Destination/Destination.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import SectionHeading from '../../Helper/SectionHeading'
+import DestinationSlider from './DestinationSlider'
 
 const Destination = () => {
   return (
     <div className='pt-20 pb-20'>
+      {/* Section Heading */}
       <SectionHeading heading='Exploring Popular Destinations' />
+      {/* Section Content */}
+      <div className='mt-14 w-[80%] mx-auto'>
+        {/* Slider */}
+        <DestinationSlider/>
+      </div>
     </div>
   )
 }
