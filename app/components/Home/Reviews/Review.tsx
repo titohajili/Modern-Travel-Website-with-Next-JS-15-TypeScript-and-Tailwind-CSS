33d49585084a18350b036776cb5a296733f86cb8
titohajili <maqatoto963@gmail.com>
2025-05-12 21:41:57 +0400

    Ratings

--- app/components/Home/Reviews/Review.tsx
+++ app/components/Home/Reviews/Review.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaStar } from 'react-icons/fa'
 
 const Review = () => {
   return (
@@ -8,7 +9,22 @@ const Review = () => {
         <div>
             <h1 className='text-2xl font-semibold text-white'>What our customers are saying us ?</h1>
             <p className='mt-6 text-gray-200'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi facilis perspiciatis maiores reprehenderit recusandae. Molestias quam dolorem officiis!</p>
+            {/* Ratings */}
+            <div className='mt-6 flex items-center space-x-6'>
+              <div>
+                <p className='text-2xl font-bold text-white'>4.88</p>
+                <p className='text-white mb-2'>Overall Ratinng</p>
+                <div className='flex items-center'>
+                  <FaStar className='text-white'/>
+                  <FaStar className='text-white'/>
+                  <FaStar className='text-white'/>
+                  <FaStar className='text-white'/>
+                  <FaStar className='text-white'/>
+                </div>
+              </div>
+            </div>
         </div>
+        {/*  */}
       </div>
     </div>
   )
