3c2a532dbec858f0a410adb7487f6512f1c1b3cc
titohajili <maqatoto963@gmail.com>
2025-06-16 20:50:20 +0400

    Email Button

--- app/components/Home/Newsletter/Newslatter.tsx
+++ app/components/Home/Newsletter/Newslatter.tsx
@@ -8,6 +8,9 @@ const Newslatter = () => {
       <h1 className='text-lg sm:text-xl md:text-2xl text-white font-semibold mt-10 tracking-widest'>Your Travel Journet Starts Here</h1>
       <p className='mt-3 text-white text-xs sm:text-sm'>Sigin up and we'll send the best deals to you</p>
       {/* subscription input and buttons */}
+      <div className='w-full'>
+        <input type="text" className='px-6 py-3.5 bg-white mt-8 w-[95%] md:w-[60%] lg:w-[40%] block mx-auto rounded-lg outline-none' placeholder='Email' />
+      </div>
     </div>
   )
 }
