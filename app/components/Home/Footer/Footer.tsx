205a9c404585f16d9886169798c759a6051423d9
titohajili <maqatoto963@gmail.com>
2025-06-16 21:06:22 +0400

    Footer st part

---
+++ app/components/Home/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='pt-16 pb-16 '>
+      <div className='w-[80%] mx-auto items-start grid-cols-1 sm:grid-cols-2 grid md:grid-cols-2 lg:grid-cols-4 gap-10'>
+        {/* 1st part */}
+        <div className='space-y-5'>
+            <h1 className='text-lg font-bold'>Company</h1>
+            <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>About Us</p>
+            <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Careers</p>
+            <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Blogs</p>
+            <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Gift Cards</p>
+            <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Magazine</p>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import {Poppins} from "next/font/google";
 //import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "./components/Home/Navbar/ResponsiveNav";
+import Footer from "./components/Home/Footer/Footer";
 
 const font = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
         <ResponsiveNav/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
