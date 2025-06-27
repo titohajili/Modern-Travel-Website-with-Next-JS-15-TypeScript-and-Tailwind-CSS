d2f94934d33991c2c9b8ecf1b8e21a4713c872fc
titohajili <maqatoto963@gmail.com>
2025-06-27 22:55:47 +0400

    Support

--- app/components/Home/Footer/Footer.tsx
+++ app/components/Home/Footer/Footer.tsx
@@ -13,6 +13,17 @@ const Footer = () => {
             <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Gift Cards</p>
             <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Magazine</p>
         </div>
+
+        {/* 2nd part */}
+
+        <div className='space-y-5'>
+            <h1 className='text-lg font-bold'>Support</h1>
+            <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Contact</p>
+            <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Legal Notice</p>
+            <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Privacy Policy</p>
+            <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Terms & Conditions</p>
+            <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Sitemap</p>
+        </div>
         
       </div>
     </div>
